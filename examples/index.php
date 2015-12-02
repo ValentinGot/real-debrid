714d1de8b223b825527d7c2e0e8fc3ac6c49cdfd
Valentin GOT <gotvalentin@gmail.com>
2015-12-02 11:57:28 +0100

    chore(examples): update

--- examples/index.php
+++ examples/index.php
@@ -1,7 +1,7 @@
 <?php
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
 $dotenv->load();
 
 header('Content-Type: application/json');
