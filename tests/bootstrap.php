90dcda7568c90243d8e62f157a5261d794a0fe58
Valentin GOT <gotvalentin@gmail.com>
2015-12-02 16:16:35 +0100

    chore(PHPUnit): skeleton

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,14 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
+require 'RealDebridSingleton.php';
 
 $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
 $dotenv->load();
 
 function getToken() {
     return getenv('TOKEN');
+}
+
+function getRealDebrid() {
+    return RealDebridSingleton::getInstance();
 }
