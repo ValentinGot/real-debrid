f5aa0dbba984f795a96b09c590b35cddcd05b003
Valentin GOT <gotvalentin@gmail.com>
2015-12-02 19:54:11 +0000

    chore(PHPUnit): User tests

--- tests/Api/UserTest.php
+++ tests/Api/UserTest.php
@@ -17,11 +17,21 @@ class UserTest extends \PHPUnit_Framework_TestCase {
     }
 
     function testGet() {
-        $client = mock_client(200, '{}');
+        $client = mock_client(200, '{
+            "id": 1,
+            "username": "ValentinGot",
+            "email": "gotvalentin@gmail.com",
+            "points": 950,
+            "locale": "fr",
+            "avatar": "https://cdn.real-debrid.com/images/avatars/214239_1448981351.1865.png",
+            "type": "premium",
+            "premium": 4320729,
+            "expiration": "2016-01-21T19:37:40.000Z"
+        }');
+
         $realDebrid = new RealDebrid(new Token(getToken()), $client);
+        $response = $realDebrid->user->get();
 
-        $res = $realDebrid->user->get();
-
-        $this->assertInternalType('object', $res);
+        $this->assertInstanceOf('RealDebrid\Response\User', $response);
     }
 }
