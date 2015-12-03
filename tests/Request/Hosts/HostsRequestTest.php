acbf81167d85baafb20a11cb0165c8b74b433610
Valentin GOT <gotvalentin@gmail.com>
2015-12-03 09:56:20 +0100

    test(HostsRequest): add tests forum /hosts namespace
    - GET /hosts
    - GET/hosts/domains
    - GET /hosts/regex
    - GET /hosts/status

---
+++ tests/Request/Hosts/HostsRequestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use RealDebrid\Auth\Token;
+use RealDebrid\Request\Hosts\HostsRequest;
+use RealDebrid\Request\RequestType;
+
+class HostsRequestTest extends \PHPUnit_Framework_TestCase {
+
+    public function testHostsRequest() {
+        $request = new HostsRequest(new Token(getToken()), 1);
+
+        $this->assertEquals(RequestType::GET, $request->getRequestType());
+        $this->assertEquals('hosts', $request->getUri());
+    }
+}
---
+++ tests/Request/Hosts/StatusRequestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use RealDebrid\Auth\Token;
+use RealDebrid\Request\Hosts\StatusRequest;
+use RealDebrid\Request\RequestType;
+
+class StatusRequestTest extends \PHPUnit_Framework_TestCase {
+
+    public function testStatusRequest() {
+        $request = new StatusRequest(new Token(getToken()), 1);
+
+        $this->assertEquals(RequestType::GET, $request->getRequestType());
+        $this->assertEquals('hosts/status', $request->getUri());
+    }
+}
